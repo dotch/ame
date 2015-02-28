3fc3d71e3056e120eaf7224545f80c927646a7fc
dotch <christian.x.weiss@gmail.com>
2015-03-01 00:01:01 +0100

    smaller dataset

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.set('port', (process.env.PORT || 9000));
 
 app.use(express.static(__dirname + "/public/"));
 
@@ -88,7 +89,7 @@ function parseData(rawData) {
 readData(function() {
   console.log('data loaded');
   // transmitLoop();
-  http.listen(9000, function() {
-    console.log('listening on 9000');
+  http.listen(app.get('port'), function() {
+    console.log('listening on ' + app.get('port'));
   })
 });
