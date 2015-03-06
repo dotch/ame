51e8f70d865e5cdf93f25c7db925e149b65e4dd4
dotch <christian.x.weiss@gmail.com>
2015-03-06 17:57:51 +0100

    factor our data extraction

--- server.js
+++ server.js
@@ -12,83 +12,38 @@ app.use(express.static(__dirname + "/public/"));
 
 io.on('connection', function(socket){
   console.log('a user connected');
-
   socket.on('startEyeDataStrean', function(qId) {
     console.log('start');
     transmitLoop(qId);
   });
-  socket.on('loadQuestion', function(qId) {
-    fs.readFile(path.join(__dirname, 'survey', qId + '_stripped.html'), 'utf8', function(err, data) {
-      socket.emit('questionContent', data);
-    });
-  });
 });
 
-var eyeData = {};
-var currentQuestion;
+var eyeData;
 var currentPos;
 var interval;
 
 function transmit() {
-  if (currentPos > eyeData[currentQuestion].length - 1) {
+  if (currentPos > eyeData.length - 1) {
     clearInterval(interval);
     console.log('stop');
     return;
   }
   var data = {
-    // eyePos = [153,154]
-    // fixationPos = [45,46]
-    x: eyeData[currentQuestion][currentPos][45],
-    y: eyeData[currentQuestion][currentPos][46]
-  }
+    x: eyeData[currentPos]['FixationPointX (MCSpx)'],
+    y: eyeData[currentPos]['FixationPointY (MCSpx)']
+  };
+  console.log(data);
   io.sockets.emit('frame', data);
   currentPos++;
 }
 
 function transmitLoop(qId) {
-  currentQuestion = qId;
   currentPos = 0;
+  eyeData = JSON.parse(fs.readFileSync(__dirname+'/data/'+qId+'/68.json', "utf8")).data;
   clearInterval(interval);
   interval = setInterval(transmit, 1000 / 120); //120hz
 }
 
-function readData(callback) {
-  parser = csv.parse({delimiter: ';'}, function(err, data){
-    parseData(data);
-    callback();
-  });
-  fs.createReadStream(__dirname+'/2012_1_TP68.csv').pipe(parser);
-}
-
-function parseData(rawData) {
-  var fieldIndex = 47;
-  for (var i = 1; i < rawData.length; i++) { // ignore first row
-    // find first question field that is not empty
-    for (var j = fieldIndex; (j < fieldIndex + 10) && (j < 146); j++) {
-      if(rawData[i][j] !== '' ) { // can be 0
-        var questionId = rawData[0][fieldIndex].slice(4).split('_')[0];
-        if (!eyeData[questionId]) {
-          eyeData[questionId] = [];
-        }
-        eyeData[questionId].push(rawData[i]);
-        fieldIndex = j;
-        break;
-      }
-    }
-  }
-  var sum = 0;
-  // verification
-  for (var key in eyeData) {
-    console.log(key, eyeData[key].length);
-    sum += eyeData[key].length;
-  }
-  console.log(sum);
-}
-
-readData(function() {
-  console.log('data loaded');
-  // transmitLoop();
-  http.listen(app.get('port'), function() {
-    console.log('listening on ' + app.get('port'));
-  });
+http.listen(app.get('port'), function() {
+  console.log('listening on ' + app.get('port'));
 });
