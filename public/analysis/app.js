d7483a94070aef9960eefbf02356f31c892ebf77
dotch <christian.x.weiss@gmail.com>
2015-04-17 14:29:25 +0200

    charts for question anaalysis

--- public/analysis/app.js
+++ public/analysis/app.js
@@ -5,10 +5,16 @@ function getData() {
 }
 
 function createPlots(data) {
-  console.log(data[13]);
-  var question = _.sortByAll(data[13],['backJumpsCount', 'fixationQuestionTextCount']);
+  data.forEach(createPlot);
+}
+
+function createPlot(questionData) {
+  var question = _.sortByAll(questionData,['backJumpsCount', 'fixationQuestionTextCount']);
   var chart = c3.generate({
-    bindto: '#graph',
+    size: {
+      height: 500,
+      width: 500
+    },
     data: {
       json: question,
       keys: {
@@ -19,30 +25,35 @@ function createPlots(data) {
       color: function (color, d) {
         // d will be 'id' when called for legends
         if (!d.id) { return; }
-        var testIndex = question.map(function(el, index) {
+        // get the index by searching for a match on
+        // backJumps and fixation count
+        var index = question.map(function(el, index) {
           if (el.backJumpsCount === d.x && el.fixationQuestionTextCount == d.value) {
             return index;
           }
-        }).filter(isFinite);
-        console.log(testIndex, d.index);
-        var participant = question[testIndex];
-        console.log(participant.backJumpsCount === d.x && participant.fixationQuestionTextCount == d.value);
+        }).filter(isFinite)[0];
+        var participant = question[index];
         return participant.problem ? d3.rgb('#ff0000') : d3.rgb('#00ff00');
       }
     },
     axis: {
       y: {
-        padding: {
-          bottom: 0
-        }
+        padding: { bottom: 0 },
+        label: 'Fixations on Question Text'
       },
       x: {
-        tick: {
-          fit: false
-        }
+        tick: { fit: false },
+        label: 'Back Jumps'
       }
-    }
+    },
+    legend: { show: false, },
+    tooltip: { show: false, },
+    point: { r: 3.5 }
   });
+  var headline = document.createElement('h3');
+  headline.innerHTML = questionData[0].question;
+  document.body.appendChild(headline);
+  document.body.appendChild(chart.element);
 }
 
 getData().then(createPlots);
